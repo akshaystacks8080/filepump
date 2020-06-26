d905f775a2d9ccb219852ccbc9e2dc3adda7950a
Akshay Pednekar <akshay.pednekar@gmail.com>
2020-06-26 15:37:56 +0530

    Fixed Formatting

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
 from flask import Flask, render_template
-from flask import request 
+from flask import request
 app = Flask(__name__,)
+
 
 @app.route('/')
 def hello_world():
     return render_template("home.html")
 
+
 @app.route("/home")
 def home_handler():
     return render_template("home.html")
 
-@app.route("/signup",  methods=['GET','POST'])
+
+@app.route("/signup",  methods=['GET', 'POST'])
 def signup():
-    if request.method =="POST":
+    if request.method == "POST":
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
-        print(name,email,password)
-        return render_template('signup.html',message="Data Recieved")
+        print(name, email, password)
+        return render_template('signup.html', message="Data Recieved")
     return render_template("/signup.html", message="")
 
-@app.route("/login", methods=['GET','POST'])
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    if request.method =="POST":
+    if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
-        print(email,password)
-        return render_template('login.html',message="Logged In Sucessfully")
+        print(email, password)
+        return render_template('login.html', message="Logged In Sucessfully")
     return render_template("/login.html", message="")
-    
+
 
 @app.route("/about")
-def about():   
+def about():
     return "This is the about page"
--- static/main.js
+++ static/main.js
@@ -1,18 +1,17 @@
 function signupClick() {
-    let curLink = window.location.href;
-    let newLink =curLink + "signup"
-    window.location.href = newLink;
+  let curLink = window.location.href;
+  let newLink = curLink + "signup";
+  window.location.href = newLink;
 }
 
 function loginClick() {
-    let curLink = window.location.href;
-    let newLink =curLink + "login"
-    window.location.href = newLink;
+  let curLink = window.location.href;
+  let newLink = curLink + "login";
+  window.location.href = newLink;
 }
 
 function aboutClick() {
-    let curLink = window.location.href;
-    let newLink = curLink + "about"
-    window.location.href = newLink;
-
+  let curLink = window.location.href;
+  let newLink = curLink + "about";
+  window.location.href = newLink;
 }
